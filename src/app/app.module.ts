ab9099fbe6e1044fdd0bd9c15698c89b941f5547
dcumpah <heartdan@hotmail.com>
2021-01-10 00:47:28 -0500

    1(b) crear componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
+import { CursoPageComponent } from './modules/home/pages/curso-page/curso-page.component';
+import { ContactoPageComponent } from './modules/home/pages/contacto-page/contacto-page.component';
+import { AdminPageComponent } from './modules/home/pages/admin-page/admin-page.component';
+import { AutenticacionPageComponent } from './modules/home/pages/autenticacion-page/autenticacion-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CursoPageComponent,
+    ContactoPageComponent,
+    AdminPageComponent,
+    AutenticacionPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/home/pages/autenticacion-page/autenticacion-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autenticacion-page',
+  templateUrl: './autenticacion-page.component.html',
+  styleUrls: ['./autenticacion-page.component.scss']
+})
+export class AutenticacionPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
