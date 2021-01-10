fe7d8925d482ec09fc47164cd4ebcc0e88915353
dcumpah <heartdan@hotmail.com>
2021-01-10 01:14:51 -0500

    1(b) crear componentes

--- src/app/modules/curso/curso.module.ts
+++ src/app/modules/curso/curso.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursoRoutingModule } from './curso-routing.module';
+import { CursoPageComponent } from './pages/curso-page/curso-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CursoPageComponent],
   imports: [
     CommonModule,
     CursoRoutingModule
